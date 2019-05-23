abb7859f8e7e3055cd1ac282d127facbece0b005
angelo.varrati@outlook.it <angelo.varrati@outlook.it>
2019-05-23 18:21:48 +0200

    soluzione con all'interno due progetti: verifica validità e blockchain "full"

---
+++ BlockChainVerificaValidita/Blocco.cs
@@ -0,0 +1,64 @@
+﻿using System;
+//libreria per la crittografia
+using System.Security.Cryptography;
+//libreria per la formattazione del testo
+using System.Text;
+
+namespace BlockChain
+{
+    class Blocco
+    {
+        //indice del blocco
+        public int Indice { get; set; }
+
+        //data e ora fino a ms
+        public DateTime DataOra { get; set; }
+
+        //chiave di cifratura del blocco precedente
+        public string HashPrecedente { get; set; }
+
+        //chiave di cifratura del blocco precedente
+        public string HashAttuale { get; set; }
+
+        //dati di transazione (mittente, destinatario, importo)
+        public string DatiTransazione { get; set; }
+
+        //assicura che i dati scambiati non possano alterati (cfr. Nonce Cryptography https://it.wikipedia.org/wiki/Nonce
+        private int Nonce { get; set; } = 0;
+
+        public Blocco(DateTime dataOra, string hashPrecedente, string transazione)
+        {
+            Indice = 0;
+            DataOra = dataOra;
+            HashPrecedente = hashPrecedente;
+            DatiTransazione = transazione;
+            HashAttuale = CalcolaHash();
+        }
+
+        public string CalcolaHash()
+        {
+            SHA256 cifraturaSHA256 = SHA256.Create();
+
+            //byte[] byteInput = Encoding.ASCII.GetBytes($"{DataOra} - {HashPrecedente ?? ""} - {DatiTransazione}");
+
+            //TODO: DEBUG NON STAMPA "NONCE"
+            byte[] byteInput = Encoding.ASCII.GetBytes($"{DataOra}-{HashPrecedente ?? ""}-{DatiTransazione}-{Nonce}");
+            byte[] byteOutput = cifraturaSHA256.ComputeHash(byteInput);
+
+            return Convert.ToBase64String(byteOutput);
+        }
+
+        public void Mina(int difficolta)
+        {
+
+            string zeroIniziali = new string('0', difficolta);
+            while (HashAttuale == null || HashAttuale.Substring(0, difficolta) != zeroIniziali)
+            {
+                Nonce++;
+                HashAttuale = CalcolaHash();
+            }
+        }
+
+    }
+}
+
---
+++ BlockChainVerificaValidita/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+//libreria per la gestione dei json
+using Newtonsoft.Json;
+
+namespace BlockChain
+{
+    class Program
+    {
+        private static void Main()
+        {
+            DateTime tempoInizio = DateTime.Now;
+
+            //instanzia un oggetto che rappresenta il mining della moneta
+            BlockChain unimolCoin = new BlockChain();
+
+            //aggiunge 4 blocchi alla blochchain con un numero di monete variabile
+            unimolCoin.AggiungiBlocco(new Blocco(DateTime.Now, null, "{mittente:Angelo,ricevente:Giusy,ammontare:10}"));
+            unimolCoin.AggiungiBlocco(new Blocco(DateTime.Now, null, "{mittente:Giusy,ricevente:Angelo,ammontare:5}"));
+            unimolCoin.AggiungiBlocco(new Blocco(DateTime.Now, null, "{mittente:Carmen,ricevente:Giusy,ammontare:5}"));
+            unimolCoin.AggiungiBlocco(new Blocco(DateTime.Now, null, "{mittente:Giusy,ricevente:Carmen,ammontare:10}"));
+
+            DateTime tempoFine = DateTime.Now;
+
+            //deserializza serializza l'oggetto in formato JSON e lo stampa
+            Console.WriteLine(JsonConvert.SerializeObject(unimolCoin, Formatting.Indented));
+
+            //inseririsci i dati relativi alla fine dell'operazione per generare i blocchi
+            // tempo impiegato: 3 blocchi SHA256 : 00:00:00.1395926
+            // tempo impiegato: 3 blocchi SHA384 : 00:00:00.0903751
+            Console.WriteLine($"Durata: {tempoFine - tempoInizio}");
+
+            //controlla se la catena è valida sugli hash
+            //  oracolo: true
+            Console.WriteLine($"La catena è valida: {unimolCoin.IsValido()}");
+
+            //controlla se la catena è valida sugli hash
+            //  oracolo: false perché ho aggiornato l'ammontare del blocco 1 senza aggiornare l'hash della blockchain
+            Console.WriteLine($"\nAggiornamento dell'ammontare a 1000 in corso...");
+            unimolCoin.Catena[1].DatiTransazione = "{sender:Giusy,receiver:Angelo,amount:1000}";
+            Console.WriteLine($"La catena è valida: {unimolCoin.IsValido()}");
+
+            //aggiornamento hash del blocco modificato
+            Console.WriteLine($"\nAggiornamento dell'Hash in corso...");
+            unimolCoin.Catena[1].HashAttuale = unimolCoin.Catena[1].CalcolaHash();
+
+            //  oracolo: true
+            Console.WriteLine($"La catena è valida: {unimolCoin.IsValido()}");
+
+            //aggiornamento degli hash dei restanti blocchi della catena per eliminare problemi di incongruenza
+            Console.WriteLine($"\nAggiornamento dell'intera catena in corso...");
+            unimolCoin.Catena[2].HashPrecedente = unimolCoin.Catena[1].HashAttuale;
+            unimolCoin.Catena[2].HashAttuale = unimolCoin.Catena[2].CalcolaHash();
+            unimolCoin.Catena[3].HashPrecedente = unimolCoin.Catena[2].HashAttuale;
+            unimolCoin.Catena[3].HashAttuale = unimolCoin.Catena[3].CalcolaHash();
+            unimolCoin.Catena[4].HashPrecedente = unimolCoin.Catena[3].HashAttuale;
+            unimolCoin.Catena[4].HashAttuale = unimolCoin.Catena[4].CalcolaHash();
+
+            //  oracolo: true
+            Console.WriteLine($"La catena è valida: {unimolCoin.IsValido()}");
+
+            //aspetta la pressione di un tasto per la terminazione del programma
+            Console.WriteLine("\nEsecuzione terminata.\nPremere un tasto per uscire...");
+
+        }
+    }
+}
